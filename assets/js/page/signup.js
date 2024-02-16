6ec7c2a70456d4ea1a0aa37c4f8e7f4b8981f266
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-02-16 22:18:10 +0700

    refactor: update validate input form signup

--- assets/js/page/signup.js
+++ assets/js/page/signup.js
@@ -20,14 +20,47 @@ const rules = {
   confirm_password: {
     required: true,
     minlength: 8,
-    equal_to: 'password'
+    equal_to: "password",
+  },
+};
+
+const methodsRule = {
+  required: (valueInput, paramsInput) => {
+    console.log("required running");
+    return valueInput !== "";
+  },
+  minlength: function (valueInput, paramsInput) {
+    console.log("minlength running");
+    return valueInput.lenth >= paramsInput;
+  },
+  email: function (valueInput, paramsInput) {
+    console.log("email running");
+    return regexEmail.test(valueInput);
+  },
+  equal_to: function (valueInput, paramsInput) {
+    console.log("equal_to running");
+    let passSelector = document.querySelector('.' + paramsInput);
+    let valuePass = passSelector.value;
+    return valuePass === valueInput;
   },
 };
 
 //======================= Start Listener Function======================
 function handleSignUpClick(event) {
   event.preventDefault();
-  console.log("click button");
+  //loop qua tung phan tu input validate
+  for (const keyNameInput in rules) {
+    let inputElement = document.querySelector("." + keyNameInput);
+    let valueInput = inputElement.value;
+    console.log(inputElement);
+    let ruleAllForInput = rules[keyNameInput];
+    //loop qua tung rule validate cua input day
+    for (const ruleItemKey in ruleAllForInput) {
+      let paramsInput = ruleAllForInput[ruleItemKey];
+      let result = methodsRule[ruleItemKey](valueInput, paramsInput);
+      console.log("result", result);
+    }
+  }
 }
 
 //3. Them su kien cho phan tu
