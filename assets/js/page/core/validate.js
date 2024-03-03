394fade89636e7cdd36635b125d96e87ef66b078
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-03-03 11:37:45 +0700

    feat: show error input

--- assets/js/page/core/validate.js
+++ assets/js/page/core/validate.js
@@ -1,23 +1,27 @@
 function Validate(options) {
+  const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   //1. Lay ra container bao dong form
   const container = document.querySelector(options.container);
   //2. Tat ca cac elements khac query dua vao container
   const btnSignUpSelector = container.querySelector(".btn-signup");
   const rules = options.rules;
   const message = options.message;
+  let errors;
 
   const rulesMethod = {
     required: function (valueInput, valueRule) {
-      console.log("valueRule", valueRule);
+      return valueInput !== "";
     },
     minlength: function (valueInput, valueRule) {
-      console.log("valueRule", valueRule);
+      return valueInput.length >= valueRule;
     },
     email: function (valueInput, valueRule) {
-      console.log("valueRule", valueRule);
+      return regexEmail.test(valueInput);
     },
     equal_to: function (valueInput, valueRule) {
-      console.log("valueRule", valueRule);
+      let passSelector = container.querySelector("." + valueRule);
+      let valuePass = passSelector.value;
+      return valueInput === valuePass;
     },
   };
 
@@ -27,20 +31,37 @@ function Validate(options) {
 
   function handleSignUpClick(event) {
     event.preventDefault();
+    errors = [];
     for (const keyInputName in rules) {
       let inputSelector = container.querySelector("." + keyInputName);
       let valueInput = inputSelector.value;
-      console.log("valueInput", valueInput);
-      console.log("keyInputName", keyInputName);
-      console.log("rules for item input", rules[keyInputName]);
 
       rulesAllForInputItem = rules[keyInputName];
 
       for (const rulesItemKey in rulesAllForInputItem) {
         let valueRule = rulesAllForInputItem[rulesItemKey];
-        rulesMethod[rulesItemKey](valueInput, valueRule);
+        let result = rulesMethod[rulesItemKey](valueInput, valueRule);
+        let keyMessage = keyInputName + '_' + rulesItemKey;
+        if(!result){
+          //đẩy lỗi vào biến đang lưu trữ
+          errors.push({
+            elemnetError: inputSelector,
+            message: message[keyMessage] ? message[keyMessage] : keyInputName + ' not valid' 
+          });
+          
+          break;
+        }
       }
     }
+
+    //hiển thị lỗi
+    errors.forEach(function(element){
+      let inputElement = element.elemnetError;
+      let divError = inputElement.nextElementSibling;
+      inputElement.classList.add('error');
+      divError.textContent = element.message;
+    });
+
   }
 
   //add event listener + data init
