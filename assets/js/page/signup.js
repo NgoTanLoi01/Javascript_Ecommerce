ec3b75d14d5f6180ab138e3b30ba4e0a46d2553a
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-03-22 11:29:06 +0700

    refactor: check email already exists in localStorage

--- assets/js/page/signup.js
+++ assets/js/page/signup.js
@@ -24,23 +24,43 @@ let signupInstanceValidate = new Validate({
     email_required: "Email không được để trống",
     email_regex: "Email không đúng định dạng",
   },
-  success: function(){
+  success: function () {
     //1. Lay du lieu tu input
     let dataForm = {};
-
-    //1.chua co thong tin user trong localStorage
-    let users =  JSON.parse(localStorage.getItem('users')) || [];
-    console.log(users);
-
-    document.querySelectorAll('.form_register input').forEach(function(element){
-      if(element.name !=='confirm_password'){
-        dataForm[element.name] = element.value;
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    //check email is exit
+    const email = document.querySelector(".form_register .email").value;
+    //Su dung some de check email trung nhau
+    let isEmailExit = users.some(
+      function (element) {
+        return element.email === email;
       }
-    });
-    //2.1 Create data users array
-    dataForm['id'] = crypto.randomUUID();
-    users.push(dataForm);
-    //2.2 Save to localStorage
-    localStorage.setItem('users', JSON.stringify(users));
-  }
+    );
+
+    //Check thu cong email trung nhau
+    // false;
+    // for (let i = 0; i < users.length; i++) {
+    //   if (users[i].email === email) {
+    //     isEmailExit = true;
+    //     break;
+    //   }
+    // }
+    console.log("isEmaiExit", isEmailExit);
+
+    //Neu email chua ton tai thi them user vao local
+    if (!isEmailExit) {
+      document
+        .querySelectorAll(".form_register input")
+        .forEach(function (element) {
+          if (element.name !== "confirm_password") {
+            dataForm[element.name] = element.value;
+          }
+        });
+      //2.1 Create data users array
+      dataForm["id"] = crypto.randomUUID();
+      users.push(dataForm);
+      //2.2 Save to localStorage
+      localStorage.setItem("users", JSON.stringify(users));
+    }
+  },
 });
