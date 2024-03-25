3001b480310a213596d62cf69da8ccf37ba19cb9
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-03-25 11:57:29 +0700

    refactor: Use Map udpate login status

--- assets/js/page/login.js
+++ assets/js/page/login.js
@@ -27,15 +27,15 @@ function validateSucsess() {
   //2. so sanh email va pass voi tat ca users trong he thong
   const users = JSON.parse(localStorage.getItem("users")) || [];
   if (users.length) {
-    users.forEach(function (element) {
+    const usersUpdate = users.map(function (element) {
       if (element.email === email && element.password === password) {
-        element.status = "active";
+        return { ...element, status: "active" };
       } else {
-        element.status = "";
+        return { ...element, status: "" };
       }
     });
     //3. cap nhat vao localStorage
-    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("users", JSON.stringify(usersUpdate));
     //4. chuyen den man hinh admin or home
     window.location.href = '/my-account.html';
   }
